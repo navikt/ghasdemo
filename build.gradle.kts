ed225a24fb18adc963803bbb73a885bb038e4386
J-K. Solbakken <jksolbakken@gmail.com>
2023-05-08 12:37:21 +0200

    the vulns just keep on coming

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val commonstext_version: String by project
+val opentsdb_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.21"
@@ -27,6 +28,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.apache.commons:commons-text:$commonstext_version")
+    implementation("net.opentsdb:opentsdb:$opentsdb_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
