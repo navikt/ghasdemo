e3ef83e280ff2e4f0614edefc74a59d644fea1eb
J-K. Solbakken <jksolbakken@gmail.com>
2023-05-10 15:09:53 +0200

    add vuln dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    //implementation("org.apache.commons:commons-text:$commonstext_version")
+    implementation("org.apache.commons:commons-text:$commonstext_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
