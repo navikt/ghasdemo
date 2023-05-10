52d35cff9ee4ad017d0cb187c59f457109ec730a
J-K. Solbakken <jksolbakken@gmail.com>
2023-05-10 13:04:17 +0200

    resolved merge conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val commonstext_version: String by project
-val opentsdb_version: String by project
+val ktor_version = "2.2.4"
+val kotlin_version = "1.8.21"
+val logback_version = "1.2.11"
+val commonstext_version = "1.9"
 
 plugins {
     kotlin("jvm") version "1.8.21"
@@ -28,7 +27,6 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.apache.commons:commons-text:$commonstext_version")
-    implementation("net.opentsdb:opentsdb:$opentsdb_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
